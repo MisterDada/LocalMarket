6b6f205ed303851bc2d3b124389e1bb6c48989fe
Obafemi <dadaobafemi26@gmail.com>
2025-08-13 14:40:58 +0100

    created categories component

--- app/Screens/Home.tsx
+++ app/Screens/Home.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import {
   FlatList,
   SafeAreaView,
+  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
+import Filters from "../Components/Categories";
 
 export default function Index() {
   const [products, setProducts] = useState([]);
@@ -23,7 +26,6 @@ export default function Index() {
       );
 
       const data = await res.json();
-      console.log(data);
       if (!data?.data.length) {
         console.log("No products found");
         setProducts([]);
@@ -69,6 +71,34 @@ export default function Index() {
           />
         </View>
       </View>
+      <View style={{ paddingHorizontal: 30 }}>
+        <View style={styles.news}>
+          <View style={{ gap: 20, maxWidth: "40%" }}>
+            <Text style={{ fontSize: 30, color: "white" }}>
+              Clearance Sales
+            </Text>
+            <TouchableOpacity style={styles.newsButton}>
+              <Text style={{ color: "#26ab3eff" }}>% up to 50%</Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            {/*There will be an image here. Z-index of 1, so the image will overflow at the top */}
+          </View>
+        </View>
+        <View style={{ gap: 20 }}>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={{ fontSize: 20 }}>Categories</Text>
+            <Text style={{ color: "#26ab3eff", fontSize: 18 }}>See all</Text>
+          </View>
+
+          <ScrollView horizontal>
+            <Filters />
+          </ScrollView>
+        </View>
+      </View>
+
       <FlatList
         data={products}
         keyExtractor={(item) => item._id?.toString()}
@@ -84,10 +114,25 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     gap: 20,
     paddingVertical: 15,
+    marginBottom: 10,
   },
   search: {
     backgroundColor: "#6c6c6c34",
     padding: 15,
     borderRadius: 10,
+  },
+  news: {
+    backgroundColor: "#26ab3eff",
+    height: 150,
+    borderRadius: 20,
+    padding: 20,
+    marginBottom: 20,
+  },
+  newsButton: {
+    borderRadius: 20,
+    backgroundColor: "white",
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
